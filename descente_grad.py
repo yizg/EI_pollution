b2447a22eeb5c46c69e38138a1a2fe10e63b7239
Moghit Yebari <moghit.yebari@student-cs.fr>
2023-11-13 13:53:13 +0100

    descente de grad

---
+++ descente_grad.py
@@ -0,0 +1,63 @@
+def BelongsInteriorDomain(node):
+	if (node < 0):
+		return 1
+	if node == 3:
+		print("Robin")
+		return 2
+	else:
+		return 0
+
+
+def compute_gradient_descent(chi, grad, domain, mu):
+	"""This function makes the gradient descent.
+	This function has to be used before the 'Projected' function that will project
+	the new element onto the admissible space.
+	:param chi: density of absorption define everywhere in the domain
+	:param grad: parametric gradient associated to the problem
+	:param domain: domain of definition of the equations
+	:param mu: step of the descent
+	:type chi: numpy.array((M,N), dtype=float64
+	:type grad: numpy.array((M,N), dtype=float64)
+	:type domain: numpy.array((M,N), dtype=int64)
+	:type mu: float
+	:return chi:
+	:rtype chi: numpy.array((M,N), dtype=float64
+
+	.. warnings also: It is important that the conditions be expressed with an "if",
+			not with an "elif", as some points are neighbours to multiple points
+			of the Robin frontier.
+	"""
+
+	(M, N) = numpy.shape(domain)
+	# for i in range(0, M):
+	# 	for j in range(0, N):
+	# 		if domain_omega[i, j] != _env.NODE_ROBIN:
+	# 			chi[i, j] = chi[i, j] - mu * grad[i, j]
+	# # for i in range(0, M):
+	# 	for j in range(0, N):
+	# 		if preprocessing.is_on_boundary(domain[i , j]) == 'BOUNDARY':
+	# 			chi[i,j] = chi[i,j] - mu*grad[i,j]
+	# print(domain,'jesuisla')
+	#chi[50,:] = chi[50,:] - mu*grad[50,:]
+	for i in range(1, M - 1):
+		for j in range(1, N - 1):
+			#print(i,j)
+			#chi[i,j] = chi[i,j] - mu * grad[i,j]
+			a = BelongsInteriorDomain(domain[i + 1, j])
+			b = BelongsInteriorDomain(domain[i - 1, j])
+			c = BelongsInteriorDomain(domain[i, j + 1])
+			d = BelongsInteriorDomain(domain[i, j - 1])
+			if a == 2:
+				print(i+1,j, "-----", "i+1,j")
+				chi[i + 1, j] = chi[i + 1, j] - mu * grad[i, j]
+			if b == 2:
+				print(i - 1, j, "-----", "i - 1, j")
+				chi[i - 1, j] = chi[i - 1, j] - mu * grad[i, j]
+			if c == 2:
+				print(i, j + 1, "-----", "i , j + 1")
+				chi[i, j + 1] = chi[i, j + 1] - mu * grad[i, j]
+			if d == 2:
+				print(i, j - 1, "-----", "i , j - 1")
+				chi[i, j - 1] = chi[i, j - 1] - mu * grad[i, j]
+
+	return chi
